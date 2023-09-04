64a7ddf4aa2a822a76e0beeb1d1ddfa8c934ed20
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-09-04 23:46:12 +0600

    Update Prime number.c

--- Prime number.c
+++ Prime number.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 int main()
 {
-    int n;        //Declare the nummber
+    int n;                                             //Declare the nummber
     printf("Enter the number: ");
-    scanf("%d",&n);    //Initialize the number
+    scanf("%d",&n);                                    //Initialize the number
     if(n == 1){
         printf("1 is neither prime nor composite.");
         return 0;
     }
-    int count = 0;         //Declare a count variable
-    for(int i = 2; i < n; i++)  //Check for factors
+    int count = 0;                                    //Declare a count variable
+    for(int i = 2; i < n; i++)                        //Check for factors
     {
         if(n % i == 0)
             count++;
     }
-    if(count == 0)           //Check whether Prime or not
+    if(count == 0)                                   //Check whether Prime or not
     {
         printf("%d is a prime number.", n);
     }
